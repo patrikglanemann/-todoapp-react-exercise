9821dd3492fc53b1112e73d7b0377f538e8e1f6c
PatrikG <Patrik0Gn@gmail.com>
2021-07-16 14:19:06 +0200

    Change todo items to objects and set keys

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState(["FirstTodo"]);
+  const [todos, setTodos] = useState([]);
 
   function handleAddButtonClick(event) {
     event.preventDefault();
     const newTodo = event.target.todoInput.value;
-    const newTodos = [...todos, newTodo];
+
+    const newTodos = [
+      ...todos,
+      { id: Date.now(), name: newTodo, isDone: false },
+    ];
     setTodos(newTodos);
   }
 
   function addTodo() {
     const newTodoList = todos.map((todo) => {
-      return <Todo todoText={todo} />;
+      return <Todo key={todo.id} todoText={todo.name} />;
     });
     return newTodoList;
   }
