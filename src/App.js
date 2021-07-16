f58cfc3b26567e53fbf70db9aafb9cd6d53f6922
PatrikG <Patrik0Gn@gmail.com>
2021-07-16 11:33:32 +0200

    Add useState for Todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState();
+
+  function handleButtonClick() {
+    setTodos("Miau");
+  }
+
   return (
     <div className="App">
       <h1>Todo</h1>
       <input type="text"></input>
-      <button>Add</button>
+      <button onClick={handleButtonClick}>Add</button>
+      <p>{todos}</p>
     </div>
   );
 }
